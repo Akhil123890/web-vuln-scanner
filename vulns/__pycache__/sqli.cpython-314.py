06ff32055a46d635f09cf7e65c44e98a6932d045
Akhil <132749371+Akhil123890@users.noreply.github.com>
2025-11-16 23:22:11 +0530

    Create sqli.cpython-314.py

---
+++ vulns/__pycache__/sqli.cpython-314.py
@@ -0,0 +1,51 @@
++
+    ›il  ã                   óT   € ^ RI t ^ RIHtHtHtHt ^ RIt. ROt . R OtR t	R t
+RR lt
+R# )	é    N)ÚurlparseÚparse_qsÚ	urlencodeÚ
+urlunparsec                 ó4   € \         P                  ! V ^RR7      # )é   F)Ú timeoutÚverify)ÚrequestsÚget)Úurls   &ÚC:\projs\web_vuln\vulns\sqli.pyÚ	_send_getr      s   € Ü
+<Š<˜ Q¨uÔ
+5Ð5ó    c                  ó4   € \         P                  ! W^RR7      # )r   F)Údatar	   r
+   )r
+   Úpost)r
+   r   s   &&r   Ú
+_send_postr      s   € Ü
+=Š=˜°¸5Ô
+AÐAr   c                 óJ  a
+€ . pV EF€  p\          EFq  p VP                  4       R 8X  dv   \        V 4      p\         VP                  4      p WG9  d   KB  Wt,          ^ ,          V,           .Wt&   \
+        V RR7      p\
+        VP                  VR7      4      p	\        V	4      p
+MFV U
+u/ uF  q»RbK   	  pp
+VP                  V4      '       d   WÄ,          V,           MTWÄ&   \        W4      p
+V
+P                  P                  4       o
+\        ;QJ d#    V
+3R l\         4       F  '       g   K
+   RM	  RM! V
+3R l\         4       4      '       d;   TP                  R RR	T R
+VP                  4       R 8X  d   X	MT R
+VRVR
+RRR/ 4        EK{  EKt  	  EKƒ  	  V# u up
+i   \          d     EK‘  i ; i)ÚGETT)Údoseq)ÚqueryÚtestc              3   ó,   <"  € T F	  qS9   x € K
+  	  R # 5i)N© )Ú.0ÚerrÚbodys   & €r   Ú	<genexpr>Úcheck_sqli.<locals>.<genexpr>8   s   øé € Ð9©j s˜d–{«jùs   ƒFÚtypez
+SQL Injectionr
+   Ú
+attack_urlÚparamÚ payloadÚevidencez,Database error message detected in response.ÚseverityÚHigh)ÚSQL_PAYLOADSÚupperr   r   r   r   r   Ú_replacer   r   r   ÚtextÚlowerÚanyÚ
+SQL_ERRORSÚappendÚ	Exception)r
+   ÚparamsÚmethodÚfindingsr#   r$   ÚparsedÚqsÚ	new_queryÚ new_urlÚrespÚpr   r   s   &&&          @r   Ú
+check_sqlir:   "   so  ø€ Ø€Häˆß#|ˆGð
+Ø—<‘<“> UÔ*Ü% c›]FÜ! &§,¡,Ó/BØ”Ù Ø!#¥¨1¥° Õ!7Ð 8B‘IÜ )¨"°DÔ 9IÜ(¨¯©¸y¨Ó)IÓJGÜ$ WÓ-‘Dñ 06Ó6©v¨!˜všI©vDÐ6Ø;?¿8¹8ÀE¿?º? $¥+° Ö"7ÐPWD‘KÜ% cÓ0Dà—y‘y—‘Ó(ß“3Ô9­jÓ9—3—3’3Ô9­jÓ9×9Ò9Ø—O‘OØ Ø˜sØ$°·±³À5Ô1H¡gÈcØ Ø! 7Ø"Ð$RØ" Fð%ô ó ò :ô% $ñ ðB €Oùò' 7øô  ô 
+Ûð
+ús=   •;FÁAFÂ#
+F
+Â.AFÄ
+FÄ%!FÅ 6FÆ
+FÆ
+F" Æ!F" ) Ú'Ú"z
+' OR '1'='1z
+" OR "1"="1z1 OR 1=1z'--z
+') OR ('1'='1) z$you have an error in your sql syntaxzwarning: mysqlz2unclosed quotation mark after the character stringz%quoted string not properly terminatedzsqlstate[hy000]z
+pg_query()z
+sql syntax)r   )r
+   Úurllib.parser   r   r   r   Úrer(   r.   r   r   r:   r   r   r   Ú<module>r?      s/   ðÛ ß BÓ BÛ 	ò€ò€
+ò6òBö$r   
