477f17c8b673c13a62ffae47da2d15daef223255
Akhil <132749371+Akhil123890@users.noreply.github.com>
2025-11-16 23:21:31 +0530

    Create redirect.cpython-314.py

---
+++ vulns/__pycache__/redirect.cpython-314.py
@@ -0,0 +1,38 @@
++
+    ›i~   ã                   óP   € ^ RI t ^ RIHtHtHtHt . R OtRt RR ltRR lt	R	R lt
+R# )
+é    N)ÚurlparseÚparse_qsÚ	urlencodeÚ
+urlunparsezhttps://attacker.example.com/c                  ó6   € \         P                  ! V ^RVR7      # )é   F)Ú timeoutÚverifyÚallow_redirects)ÚrequestsÚget)Úurlr
+   s   &&Ú#C:\projs\web_vuln\vulns\redirect.pyÚ	_send_getr      s   € Ü
+<Š<˜ Q¨uÀoÔ
+VÐVó    c                 ó6   € \         P                  ! W^RVR7      # )r   F)Údatar	   r
+   r
+   )r   Úpost)r   r   r
+   s   &&&r   Ú
+_send_postr      s   € Ü
+=Š=˜°¸5ÐRaÔ
+bÐbr   c                 ó–  € . pV EF'  pVP                  4       \        9  d   K   VP                  4       R 8X  dh   \         V 4      p\	        VP
+                  4      pWF9  d   K]  \        .Wd&   \        VRR7      p \        VP                  V R7      4      p\        V4      p	Tp
+M-V U
+u/ uF  q»RbK   	  pp
+\        WÄ&   \        W4      p	V R,           p
+V	P                  p
+\        P                  R4      V
+P                  R4      9   d*   VP                  R RR	V R
+V
+R
+VR\        R
+RV
+ 2RR/ 4       EK'  EK*  	  V# u up
+i   \         d     EKB  i ; i)ÚGETT)Údoseq)ÚqueryÚtestz  [POST]Ú/Útypez
+Open Redirectr   Ú
+attack_urlÚparamÚ payloadÚevidencez'Redirected to attacker-controlled URL: ÚseverityÚMedium)ÚlowerÚREDIRECT_PARAMSÚupperr   r   r   ÚMALICIOUS_TARGETr   r   Ú_replacer   r   r   ÚrstripÚappendÚ	Exception)r   ÚparamsÚmethodÚfindingsr   ÚparsedÚqsÚ	new_queryÚ new_urlÚrespr   Úpr   Ú	final_urls   &&&           r   Úcheck_open_redirectr5      sK  € Ø€HäˆØ
+;‰;‹=¤Ô
+/Ùð	Ø|‰|‹~ Ô&Ü! #›Ü˜fŸl™lÓ+Ø”?ÙÜ-Ð.‘	Ü% b°Ô5	Ü$ V§_¡_¸9 _Ó%EÓF Ü  Ó)Ø$‘
+á+1Ó2©6 a˜6š	©6Ð2Ü.‘
+Ü! #Ó,Ø  9_
+ð Ÿ™ˆIÜ×&Ñ& sÓ+¨y×/?Ñ/?ÀÓ/DÔDØ—‘Ø˜OØ˜3Ø  *Ø˜UØÔ/ØÐ"IÈ)ÈÐ UØ ð!÷ ò Eñ- ðF €Oùò) 3øô" ô 	Ûð	ús+   ¥;D8Á"AD8Â%
+D3Â0A9D8Ä3D8Ä8
+EÅ E)r   ÚredirectÚnextÚdestÚ
+destination)T)r   )
+r   Úurllib.parser   r   r   r   r$   r&   r   r   r5   © r   r   Ú<module>r<      s*   ðÛ ß BÓ BâD€Ø2Ð ôWôcö&r   
