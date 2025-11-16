4becf1af372dfb9d73dfad0aa7b99da737c865ab
Akhil <132749371+Akhil123890@users.noreply.github.com>
2025-11-16 23:22:44 +0530

    Create xss.cpython-314.py

---
+++ vulns/__pycache__/xss.cpython-314.py
@@ -0,0 +1,37 @@
++
+    ›iú  ã                   óD   € ^ RI t ^ RIHtHtHtHt . ROtR t R tR R lt	R# )é    N)ÚurlparseÚparse_qsÚ	urlencodeÚ
+urlunparsec                 ó4   € \         P                  ! V ^RR7      # )é   F)Ú timeoutÚverify)ÚrequestsÚget)Úurls   &ÚC:\projs\web_vuln\vulns\xss.pyÚ	_send_getr   
+   s   € Ü
+<Š<˜ Q¨uÔ
+5Ð5ó    c                  ó4   € \         P                  ! W^RR7      # )r   F)Údatar	   r
+   )r
+   Úpost)r
+   r   s   &&r   Ú
+_send_postr      s   € Ü
+=Š=˜°¸5Ô
+AÐAr   c                 ó  € . pV Fæ  p\          FÙ  p VP                  4       R 8X  dd   \        V 4      p\         VP                  4      p WG9  d   KA  V.Wt&   \
+        V RR7      p\
+        VP                  VR7      4      p	\        V	4      p
+T	p
+M)V Uu/ uF  qÌRbK   	  p
+pW]V&   \        W
+4      p
+V R,           p
+V
+P                  pW^9   d#   VP                  RR RV R	V
+R
+VR
+VRR
+RR/ 4        Kâ  KÛ  	  Kè  	  V# u upi   \         d     Kö  i ; i)ÚGETT)Údoseq)ÚqueryÚtestz  [POST]ÚtypezCross-Site Scripting (XSS)r
+   Ú
+attack_urlÚparamÚ payloadÚevidencezPayload reflected in response.ÚseverityÚHigh)
+ÚXSS_PAYLOADSÚupperr   r   r   r   r   Ú_replacer   r   ÚtextÚappendÚ	Exception)r
+   ÚparamsÚmethodÚfindingsr   r   ÚparsedÚqsÚ	new_queryÚ new_urlÚrespr   Úpr   Úbodys   &&&            r   Ú	check_xssr1      s%  € Ø€Hãˆß#ˆGð
+Ø—<‘<“> UÔ*Ü% c›]FÜ! &§,¡,Ó/BØ”Ù Ø!( 	B‘IÜ )¨"°DÔ 9IÜ(¨¯©¸y¨Ó)IÓJGÜ$ WÓ-DØ!(‘Já/5Ó6©v¨!˜všI©vDÐ6Ø")˜‘KÜ% cÓ0DØ!$ y¥JØ—y‘yØ”?Ø—O‘OØÐ <Ø˜sØ$ jØ Ø! 7Ø"Ð$DØ" Fð%ô ò ñ #ó% $ñ ðB €Oùò' 7øô  ô 
+Úð
+ús*   ’;C6Á?C6Â
+C1ÂA
+C6Ã1C6Ã6
+D ÄD )z<script>alert(1)</script>z"'><script>alert(1)</script>z<img src=x onerror=alert(1)>)r   )
+r
+   Úurllib.parser   r   r   r   r!   r   r   r1   © r   r   Ú<module>r4      s%   ðÛ ß BÓ Bò€ò6òBö$r   
