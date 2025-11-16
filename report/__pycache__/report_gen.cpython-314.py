363c38d705eb51acafe7119e40f8241dc74cddb3
Akhil <132749371+Akhil123890@users.noreply.github.com>
2025-11-16 23:18:01 +0530

    Create report_gen.cpython-314.py

---
+++ report/__pycache__/report_gen.cpython-314.py
@@ -0,0 +1,32 @@
++
+     iÁ  ã                   ó    € ^ RI H t  ^ RItR tR# )é    )ÚdatetimeNc                 óÔ  € \         P                  ! 4       P                  R 4      p\         VRRR7      ;_uu_ 4       pVP	                  R4       VP	                  R4       VP	                  R4       VP	                  R 4       VP	                  R\
+        P                  ! V 4       R	24       VP	                  R
+V R	24       VP	                  R
+\        V4       R24       V'       g   VP	                  R
+4       EM3VP	                  R4       VP	                  R4       \        V^4       EFï  w  rVVP                  RR4      P                  4       p VP	                  R4       VP	                  RV R24       VP	                  R\
+        P                  ! VP                  RR4      4       R24       VP	                  RV  R\
+        P                  ! VP                  RR4      4       R24       VP	                  R\
+        P                  ! VP                  RR4      4       R24       VP	                  R\
+        P                  ! VP                  RR4      4       R24       VP	                  R\
+        P                  ! VP                  RR4      4       R24       VP	                  R\
+        P                  ! VP                  RR4      4       R24       VP	                  R\
+        P                  ! VP                  RR4      4       R24       VP	                  R4       EKò  	  VP	                  R4       VP	                  R 4       VP	                  R!4       R"R"R"4       R"#   + '       g   i     R"# ; i)#z%Y-%m-%d %H:%M:%SÚwzutf-8)ÚencodingzN<html><head><meta charset='utf-8'><title>Web Vulnerability Scan Report</title>a)  <style>body{font-family:Arial, sans-serif;margin:20px;} table{border-collapse:collapse;width:100%;} th,td{border:1px solid #ccc;padding:8px;text-align:left;} th{background:#eee;} .high{color:red;font-weight:bold;} .medium{color:orange;font-weight:bold;} .low{color:green;font-weight:bold;}</style>z
+</head><body>z&<h1>Web Vulnerability Scan Report</h1>z<p><b>Target:</b> z</p>z<p><b>Generated at:</b> z<p><b>Total Findings:</b> z</p><hr>z<<p>No vulnerabilities detected with current payload set.</p>z <table>zƒ<tr><th>#</th><th>Type</th><th>Severity</th><th>URL</th><th>Attack URL</th><th>Parameter</th><th>Payload</th><th>Evidence</th></tr>ÚseverityÚ z<tr>z<td>z</td>ÚtypeÚ-z
+<td class='z'>ÚurlÚ
+attack_urlÚparamÚ payloadÚevidencez</tr>z</table>z˜<hr><p><i>Note: This is a basic academic scanner. Results may include false positives or miss complex vulnerabilities. Always validate manually.</i></p>z</body></html>N)
+r   ÚnowÚstrftimeÚopenÚwriteÚhtmlÚescapeÚlenÚ	enumerateÚgetÚlower)Úbase_urlÚfindingsÚfilenamer   ÚfÚiÚissueÚseverity_classs   &&&     Ú&C:\projs\web_vuln\report\report_gen.pyÚgenerate_reportr"      s¥  € Ü
+,Š,‹.×
+!Ñ
+!Ð"5Ó
+6€Cä	
+ˆh˜ g×	.Õ	.°!Ø	 ‰ Ð`ÔaØ	 ‰ ð  |ô  	}Ø	 ‰ Ô Ø	 ‰ Ð8Ô9Ø	 ‰ Ð$¤T§[¢[°Ó%:Ð$;¸4Ð@ÔAØ	 ‰ Ð*¨3¨%¨tÐ4Ô5Ø	 ‰ Ð,¬S°«]¨O¸8ÐDÔEçØ
+G‰GÐRÖSà
+G‰GIÔØ
+G‰Gð  Zô  
+[Ü% h°×2‘Ø!&§¡¨:°rÓ!:×!@Ñ!@Ó!BØ— ‘ ˜”Ø— ‘ ˜$˜q˜c ˜Ô(Ø— ‘ ˜$œtŸ{š{¨5¯9©9°V¸SÓ+AÓBÐCÀ5ÐIÔJØ— ‘ ˜+ nÐ%5°R¼¿
+º
+ÀEÇIÁIÈjÐZ]ÓD^Ó8_Ð7`Ð`eÐfÔgØ— ‘ ˜$œtŸ{š{¨5¯9©9°U¸CÓ+@ÓAÐBÀ%ÐHÔIØ— ‘ ˜$œtŸ{š{¨5¯9©9°\À3Ó+GÓHÐIÈÐOÔPØ— ‘ ˜$œtŸ{š{¨5¯9©9°W¸cÓ+BÓCÐDÀEÐJÔKØ— ‘ ˜$œtŸ{š{¨5¯9©9°YÀÓ+DÓEÐFÀeÐLÔMØ— ‘ ˜$œtŸ{š{¨5¯9©9°ZÀÓ+EÓFÐGÀuÐMÔNØ— ‘ ˜ × ñ 3ð 
+G‰GJÔà	 ‰ ð  kô  	lØ	 ‰ Ð Ô!÷; 
+/×	.×	.Ò	.ús   »LMÍ
+M'	)r   r   r"   © ó    r!   Ú<module>r%      s   ðÝ Û 
+ô "r$   
