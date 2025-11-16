4c10660ebb31090d9d19bba04bd10cff7b295b12
Akhil <132749371+Akhil123890@users.noreply.github.com>
2025-11-16 23:15:06 +0530

    Create crawler.cpython-314.py

---
+++ __pycache__/crawler.cpython-314.py
@@ -0,0 +1,45 @@
++
+    ›i,   ã                   ó8   € ^ RI t ^ RIHt ^ RIHtHtHt RR lt R# )é    N)Ú
+BeautifulSoup)Ú urljoinÚurlparseÚparse_qsc           	     ó‚  € \        4       pV ^ 3.p. p\        V 4      P                  pRR/p V'       Edô   VP                   ^ 4      w  r‰Wƒ9   g    W‘8”  d   K*  VP	                  V4        \
+        P                  ! W‡VRR7      p
+\        T
+P                  R4      p
+\        T4      p\        \        TP                  4      P                  4       4      p
+T
+'       d   TP                  RTR T
+RR	/4       T
+P                  R
+4       Fµ  pTP
+                  RR
+4      P!                  4       pTP
+                  R4      ;'       g    Tp\#        TT4      pTP                  . RO4       Uu. uF-  pTP
+                  R
+4      '       g   K  TP
+                  R
+4      NK/  	  ppT'       g   Kž  TP                  RTR TRT/4       K·  	  T
+P                  RRR7       FW  p\#        TTR,          4      p\        T4      pTP                  T8X  g   K4  TT9  g   K=  TP                  TT	^,           34       KY  	  EKü  V#   \         d     EK
+  i ; iu upi )zˆ
+Very simple crawler that:
+- Stays within the same domain
+- Collects URLs with query parameters
+- Collects forms and their input fields
+z
+User-AgentzMiniWebScanner/1.0F)Ú headersÚ timeoutÚverifyz
+html.parserÚurlÚparamsÚmethodÚGETÚformÚgetÚactionÚnameÚaT)Úhrefr   )ÚinputÚtextareaÚselect)Úsetr   ÚnetlocÚpopÚaddÚrequestsr   Ú	Exceptionr   ÚtextÚlistr   ÚqueryÚkeysÚappendÚfind_allÚupperr   )Ú	start_urlÚ	max_depthr	   Ú visitedÚto_visitÚ targetsÚdomainr   r
+   ÚdepthÚrespÚsoupÚparsedr   r   Ú
+form_methodÚ
+form_actionÚform_urlÚiÚinputsÚlinkÚlink_urlÚ
+parsed_links   &&&                    ÚC:\projs\web_vuln\crawler.pyÚcrawlr8      sú  € ô ‹e€GØ˜AÐ€HØ€Gä
+iÓ
+ ×
+'Ñ
+'€Fð 	Ð*ð€G÷ 
+ˆ(Ø—\‘\ !“_‰
+ˆà
+Œ>˜UÔ.ÙØ
+‰
+CÔð	Ü—<’< ¸gÈeÔTˆDô ˜TŸY™Y¨
+Ó6ˆô ˜#“ˆÜ”h˜vŸ|™|Ó,×1Ñ1Ó3Ó4ˆß
+ØN‰N˜E 3¨°&¸(ÀEÐJÔKð —M‘M &Ö)ˆDØŸ(™( 8¨UÓ3×9Ñ9Ó;ˆKØŸ(™( 8Ó,×3Ð3°ˆKÜ˜s KÓ0ˆHØ-1¯]©]Ò;ZÔ-[ÓmÑ-[¨Ð_`×_dÑ_dÐek×_l”ma—e‘e˜F–mÑ-[ˆFÐmß‰vØ—‘  x°¸6À8È[ÐYÖZñ
+ *ð —M‘M #¨DMÖ1ˆDÜ˜s D¨¥LÓ1ˆHÜ" 8Ó,ˆKØ×!Ñ! VÖ+°À Ö0GØ—‘ ¨5°1­9Ð 5Ö6ô	 2ð €Nøô7 ô 	Ûð	üò  ns   Á'H) ÅH<Å:H<È)
+H9È8H9)é   é   )r   Úbs4r   Úurllib.parser   r   r   r8   © ó    r7   Ú<module>r?      s   ðÛ Ý ß 4Ñ 4ö5r>   
